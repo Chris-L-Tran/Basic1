cd0f0a700b9dd48f6a17b04bf5d54c518a7f68e3
Christopher Tran <christopher.long.tran@gmail.com>
2018-04-07 14:16:51 -0400

    got various buttons to work

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
+var dark = false
+
 function changeBackground() {
   var color = document.getElementById('background-color-input').value
   document.getElementById('button-background').style.background = color
+}
+
+function toggleColors() {
+
+  if (dark === true) {
+    document.getElementById('button-background').style.background = 'white'
+    dark = false
+  } else {
+    document.getElementById('button-background').style.background = 'grey'
+    dark = true
+  }
+}
+
+function showTime() {
+  var now = new Date()
+  var hours = now.getHours()
+  if (hours > 12) {
+    hours = hours - 12
+  }
+  var minutes = now.getMinutes()
+  alert(hours + " : " + minutes)
 }
